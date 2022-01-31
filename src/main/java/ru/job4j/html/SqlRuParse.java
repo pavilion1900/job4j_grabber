f01354811d07514161065f86711c8fc664fb2ff2
pavilion1900 <pavilion1900@gmail.com>
2022-02-01 00:08:05 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -9,14 +9,20 @@ import java.io.IOException;
 
 public class SqlRuParse {
     public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element element : row) {
-            Element child = element.child(0);
-            System.out.println(child.attr("href"));
-            System.out.println(child.text());
-            Element parent = element.parent();
-            System.out.println(parent.child(5).text());
+        for (int i = 1; i < 6; i++) {
+            String url = "https://www.sql.ru/forum/job-offers/1";
+            String[] array = url.split("/");
+            array[array.length - 1] = String.valueOf(i);
+            url = String.join("/", array);
+            Document doc = Jsoup.connect(url).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element element : row) {
+                Element child = element.child(0);
+                System.out.println(child.attr("href"));
+                System.out.println(child.text());
+                Element parent = element.parent();
+                System.out.println(parent.child(5).text());
+            }
         }
     }
 }
