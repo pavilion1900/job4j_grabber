2bd6effacf52863f59caf9aa5e70f7d6ef593b60
pavilion1900 <pavilion1900@gmail.com>
2022-02-01 16:13:43 +0300

    2.2. Модель данных - Post. [#285211 #258643]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -21,6 +21,13 @@ public class Post {
         this.created = created;
     }
 
+    public Post(String title, String link, String description, LocalDateTime created) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
     public int getId() {
         return id;
     }
@@ -50,15 +57,12 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return id == post.id
-                && Objects.equals(title, post.title)
-                && Objects.equals(link, post.link)
-                && Objects.equals(created, post.created);
+        return id == post.id && Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, link, created);
+        return Objects.hash(id, link);
     }
 
     @Override
