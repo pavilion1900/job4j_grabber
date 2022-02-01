d2732a532bf878e0becb73f70a2b440b8b9fa583
pavilion1900 <pavilion1900@gmail.com>
2022-02-02 02:18:43 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,21 +4,35 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Post;
+import ru.job4j.grabber.utils.*;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class SqlRuParse {
+    public Post getPost(String link, String title) throws IOException {
+        DateTimeParser parser = new SqlRuDateTimeParser();
+        Document page = Jsoup.connect(link).get();
+        String description = page.select(".msgBody").get(1).text();
+        String dateTime = page.select(".msgFooter").text();
+        dateTime = dateTime.substring(0, dateTime.indexOf(":") + 3);
+        LocalDateTime created = parser.parse(dateTime);
+        return new Post(title, link, description, created);
+    }
+
     public static void main(String[] args) throws IOException {
+        SqlRuParse sqlRuParse = new SqlRuParse();
         for (int i = 1; i < 6; i++) {
             String url = "https://www.sql.ru/forum/job-offers/";
             Document doc = Jsoup.connect(url + i).get();
             Elements row = doc.select(".postslisttopic");
             for (Element element : row) {
                 Element child = element.child(0);
-                System.out.println(child.attr("href"));
-                System.out.println(child.text());
-                Element parent = element.parent();
-                System.out.println(parent.child(5).text());
+                String link = child.attr("href");
+                String title = child.text();
+                Post post = sqlRuParse.getPost(link, title);
+                System.out.println(post);
             }
         }
     }
